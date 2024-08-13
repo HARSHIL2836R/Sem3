692efc98242784d50776f3bf2b5800cc286bc542
HARSHIL2836R <harshilsolanki1912@gmail.com>
2024-08-14 01:31:46 +0530

    circular vs linear implementation Seg fault in circular

--- CS213/lab02/circular-vs-linear/linearQueue.cpp
+++ CS213/lab02/circular-vs-linear/linearQueue.cpp
@@ -4,23 +4,46 @@
 using namespace std;
 
 template <typename T> bool DynamicQueue<T> :: isEmpty() {
-  return true; // dummy return
+  return (tail-head)==0; // dummy return
 }
 
 template <typename T> bool DynamicQueue<T> :: isFull() {
-  return true; // dummy return
+  return tail == N-1; // dummy return
 }
 
 template <typename T> void DynamicQueue<T> :: grow() {
+  N=nextSize();
+  T * A_new = new T[N];
+  int i = 0;
+  while (head != tail)
+  {
+    A_new[i] = A[head];
+    head++;
+    i++;
+  }
+  head = 0;
+  tail = i-1;
+  delete [] A;
+  A = A_new;
 }
 
 template <typename T> unsigned int DynamicQueue<T> :: size() {
-  return 0; // dummy return
+  return (tail-head); // dummy return
 }
 
 template <typename T> void DynamicQueue<T> :: QInsert(T x) {
+  if (isFull()){
+    grow();
+  }
+  A[++tail]=x;
 }
 
 template <typename T> bool DynamicQueue<T> :: QDelete(T* x) {
-  return true; // dummy return
+  if (isEmpty()){
+    return false;
+  }
+  else{
+    A[head++]=NULL;
+    return true;
+  }
 }
