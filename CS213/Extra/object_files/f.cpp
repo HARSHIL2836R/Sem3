2a15ee4fbc95c96e6be5fea3846ab30bb6cfb4dc
HARSHIL2836R <harshilsolanki1912@gmail.com>
2024-09-11 23:50:29 +0530

    CA work

---
+++ CS213/Extra/object_files/f.cpp
@@ -0,0 +1,8 @@
+#ifndef my_f
+#include "g.cpp"
+#define my_f = 54
+int hello(){
+    return 1;
+}
+#endif
+
---
+++ CS213/Extra/object_files/g.cpp
@@ -0,0 +1,9 @@
+
+#ifndef my_g
+#include "f.cpp"
+#define my_g = 23
+
+int main(){
+    hello();
+}
+#endif
