60133dd73b18ac922177d685ebee54f759eea753
HARSHIL2836R <harshilsolanki1912@gmail.com>
2024-10-10 19:28:43 +0530

    Add solution and do compression

--- CS213/lab07/lab07/compression/rle.cpp
+++ CS213/lab07/lab07/compression/rle.cpp
@@ -5,16 +5,85 @@ rle::rle(int gamma): gamma(gamma){}
 
 // Convert integer to gamma-bit binary string
 string rle::f(int i)
-{ return ""; }
+{
+    string s;
+    while (i > 0)
+    {
+        s = std::to_string(i % 2) + s;
+        i >>= 1;
+    }
+    return s;
+}
 
 // Convert gamma-bit binary string to integer
 int rle::g(string i)
-{ return 0; }
+{ 
+    int res = 0;
+    for (auto ch: i){
+        res <<= 1;
+        res += (ch == '1');
+    }
+    return res;
+}
 
 // Compress the input file
+string rle::compress_old(string input_string)
+{ 
+    int i = 0;
+    string res = "";
+    char ch = input_string[i];
+    while (i<input_string.length()){
+        int count = 1;
+        while (input_string[++i] == ch){
+            count++;
+        }
+        res.append(1,ch);
+        res.append(1, count+'0');
+        ch = input_string[i];
+    }
+    return res;
+}
 string rle::compress(string input_string)
-{ return ""; }
+{ 
+    int i = 0;
+    string res = "";
+    char ch = input_string[i];
+    while (i<input_string.length()){
+        int count = 1;
+        while (input_string[++i] == ch){
+            count++;
+        }
+        res.append(1,ch);
+        res.append(1, count+'0');
+        ch = input_string[i];
+    }
+    return res;
+}
 
 // Decompress the input file
+string rle::decompress_old(string input_string)
+{
+    string res = "";
+    for (int i = 0; i < input_string.length();i++){
+        char ch = input_string[i];
+        res.append(input_string[++i]-'0',ch);
+    }
+    return res;
+}
 string rle::decompress(string input_string)
-{ return ""; }
+{
+    string res = "";
+    for (int i = 0; i < input_string.length();i++){
+        char ch = input_string[i];
+        res.append(input_string[++i]-'0',ch);
+    }
+    return res;
+}
+
+/* int main(){
+    rle rle_obj(2);
+    std::cout << rle_obj.f(10) << std::endl; // Output: 1010
+    std::cout << rle_obj.g("1010") << std::endl; // Output: 10
+    std::cout << rle_obj.compress("1000111100") << std::endl; // Output: 11031402
+    std::cout << rle_obj.decompress("11031402") << std::endl; // Output: 1000111100
+} */
